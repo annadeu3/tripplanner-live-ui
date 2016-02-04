545cb8951e4651764f1ad5ee3ca34602b3819469
annadeu3 <annadeu3@gmail.com>
2016-02-04 17:31:49 -0500

    add hotels, restaurants, activities to itinerary panel with limits

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,25 @@
 
   $('#hotelbutton').on('click', function(){
-      var $selectedhotel = $('#hotelselect option:selected').text();
+  	if ($('ul#hotellist>div').length > 0) {
+  		console.log("Too many hotels!");
+  	}
+      else {
+      	var $selectedhotel = $('#hotelselect option:selected').text();
       $('<div class="itinerary-item"><span class="title">'+$selectedhotel+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>').appendTo('#hotellist');
+  	   }
    });
 
- 
+   $('#restaurantbutton').on('click', function(){
+   		if ($('ul#restaurantlist>div').length > 2) {
+  		console.log("Too many restaurants!");
+  		}
+      else {
+      var $selectedrestaurant = $('#restaurantselect option:selected').text();
+      $('<div class="itinerary-item"><span class="title">'+$selectedrestaurant+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>').appendTo('#restaurantlist');
+  	  }
+   });
+
+    $('#activitybutton').on('click', function(){
+      var $selectedactivity = $('#activityselect option:selected').text();
+      $('<div class="itinerary-item"><span class="title">'+$selectedactivity+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>').appendTo('#activitylist');
+   });
