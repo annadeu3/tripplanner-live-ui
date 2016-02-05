f106bdbc46275016424e8f40399e5cc3ba010c26
annadeu3 <annadeu3@gmail.com>
2016-02-05 10:38:42 -0500

    added map marker icons

--- public/js/main.js
+++ public/js/main.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	      	}
 	      }
 
-	   	  drawLocation(hotelLocation);
+	   	  drawLocation(hotelLocation, {icon: '../images/hotel-bed.jpg'});
   	   }
    });
 
@@ -32,15 +32,44 @@ $(document).ready(function() {
   		console.log("Too many restaurants!");
   		}
       else {
-      var $selectedrestaurant = $('#restaurantselect option:selected').text();
-      $('<div class="itinerary-item"><span class="title">'+$selectedrestaurant+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>').appendTo('#restaurantlist');
-  	  }
+        var $selectedrestaurant = $('#restaurantselect option:selected').text();
+        $('<div class="itinerary-item"><span class="title">'+$selectedrestaurant+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>').appendTo('#restaurantlist');
+    	   for(var i = 0; i < restaurants.length; i++) {
+            // console.log("current hotel name", hotels[i].name)
+            
+            if(restaurants[i].name == $selectedrestaurant) {
+              // console.log("matching hotel",hotels[i]);
+              for (var key in restaurants[i].place[0]) {
+                var restaurantLocation = restaurants[i].place[0].location; // array
+                // console.log("location is",hotelLocation);
+              }
+              break;
+            }
+          }
+
+          drawLocation(restaurantLocation, {icon: '../images/cutlery.png'});
+         }
    });
 
     $('#activitybutton').on('click', function(){
       var $selectedactivity = $('#activityselect option:selected').text();
       $('<div class="itinerary-item"><span class="title">'+$selectedactivity+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>').appendTo('#activitylist');
-   });
+       for(var i = 0; i < activities.length; i++) {
+            // console.log("current hotel name", hotels[i].name)
+            
+            if(activities[i].name == $selectedactivity) {
+              // console.log("matching hotel",hotels[i]);
+              for (var key in activities[i].place[0]) {
+                var activityLocation = activities[i].place[0].location; // array
+                // console.log("location is",hotelLocation)
+              }
+              break;
+            }
+          }
+
+          drawLocation(activityLocation, {icon: '../images/museum.png'});
+
+  });
 
 
- });
+});
